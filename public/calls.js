4bc7e9520d0b198ff9084c5c0732ae1d6d0fb912
Faris Šišić <sisicfarisofficial@gmail.com>
2020-02-13 18:00:51 +0100

    Still editing...

--- public/calls.js
+++ public/calls.js
@@ -8,30 +8,50 @@ let Calls = (function(){
   .then(json => {
       var drinks = json.drinks;
       document.getElementById("resultsDiv").innerHTML = "";
+      console.log(drinks);
       if( drinks == null  ){
+          var newDiv = document.createElement("div");
+          newDiv.style.padding = "15px";
+          newDiv.style.textAlign ="center";
           var p = document.createElement("p");
           var img = styleImg("brokenSearch.png","200","200");
           p.innerHTML = "OOPS! We couldnt't find that drink :(";
           p.classList.add("wrongSearch");
-          document.getElementById("resultsDiv").appendChild(p);
-          document.getElementById("resultsDiv").appendChild(img);
+          newDiv.appendChild(p);
+          newDiv.appendChild(img);
+          document.getElementById("resultsDiv").appendChild(newDiv);
       }
       else if(searchInput == ""){
+        var newDiv = document.createElement("div");
+        newDiv.style.padding = "15px";
+        newDiv.style.textAlign ="center";
         var p = document.createElement("p");
         var img = styleImg("brokenSearch.png","200","200");
-        p.innerHTML = "OOPS! You haven't searched for anything :(";
+        p.innerHTML = "OOPS! Empty search :(";
         p.classList.add("wrongSearch");
-        document.getElementById("resultsDiv").appendChild(p);
-        document.getElementById("resultsDiv").appendChild(img);
+        newDiv.appendChild(p);
+        newDiv.appendChild(img);
+        document.getElementById("resultsDiv").appendChild(newDiv);
       }
       else{
       for( var i = 0; i < drinks.length; i++ ){
             var newDiv = document.createElement("div");
+            newDiv.style.padding = "15px";
             var p = document.createElement("p");
             p.innerHTML = drinks[i].strDrink;
             p.classList.add("drinkName");
+            var alcoholAlertParagraph = document.createElement("p");
+            alcoholAlertParagraph.style.whiteSpace = "nowrap";
+        if( drinks[i].strAlcoholic == "Alcoholic" ){
+            alcoholAlertParagraph.innerHTML = "ALCOHOLIC";
+            alcoholAlertParagraph.classList.add("alcoholic");
+        }else{
+            alcoholAlertParagraph.innerHTML = "ALCOHOL FREE";
+            alcoholAlertParagraph.classList.add("alcoholFree");
+        }
             var img = styleImg(drinks[i].strDrinkThumb, "100", "100");
             newDiv.appendChild(p);
+            newDiv.appendChild(alcoholAlertParagraph);
             newDiv.appendChild(img);
             if( i < drinks.length -1 ) {
                 newDiv.style.borderBottom = "2px solid rgba(255,255,255,0.3)";
